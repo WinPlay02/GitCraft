9b5acb3578d97484c13998caba8038b583384f6a
Deximus-Maximus <kev12east@gmail.com>
2022-06-21 20:13:04 -0400

    Update test maybe

--- src/test/groovy/dex/mcgitmaker/McMetadataTest.groovy
+++ src/test/groovy/dex/mcgitmaker/McMetadataTest.groovy
@@ -1,20 +1,23 @@
 package dex.mcgitmaker
 
-import dex.mcgitmaker.data.McVersion
-import dex.mcgitmaker.loom.Decompiler
+
 import net.fabricmc.loader.api.SemanticVersion
 
 class McMetadataTest {
     static void main(String[] args) {
         def x = new GitCraft()
-        def v = x.versions.get(SemanticVersion.parse('1.18'))
+        def v = x.versions.get(SemanticVersion.parse('1.19.1-beta.1'))
 
         //x.merged() // has to be called or it endlessly tries to merge them
+
+        //v.remappedJar()
+        v.decompiledMc()
 
         //Decompiler.decompile(v)
         //x.decompileAllVersions(false)
         //x.updateRepo()
-        v.decompiledMc()
+        //v.decompiledMc()
+        //Util.updateMcVersionPath(v)
 
         //keyset is in ascending order, good
     }
