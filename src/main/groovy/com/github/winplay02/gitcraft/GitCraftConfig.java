bc5f3b72c443531097f12fc298701e5f9678fe6e
WinPlay02 <winplay02_gh@woberlaender.de>
2024-01-17 18:51:27 +0100

    CHANGED: Soft-require fabric-loader 0.15.6

--- src/main/groovy/com/github/winplay02/gitcraft/GitCraftConfig.java
+++ src/main/groovy/com/github/winplay02/gitcraft/GitCraftConfig.java
@@ -62,7 +62,7 @@ public class GitCraftConfig {
 	public String[] excludedVersion = null;
 
 	/// Mapping quirks
-	public static final String MIN_SUPPORTED_FABRIC_LOADER = "0.15.3";
+	public static final String MIN_SUPPORTED_FABRIC_LOADER = "0.15.6";
 
 	public static final SemanticVersion INTERMEDIARY_MAPPINGS_START_VERSION, YARN_MAPPINGS_START_VERSION, YARN_CORRECTLY_ORIENTATED_MAPPINGS_VERSION, PARCHMENT_START_VERSION;
 
