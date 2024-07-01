8ae994d7be47895752fa0cb0bf9f2a49996c54de
Space Walker <spacedoesrs@gmail.com>
2024-07-01 17:51:59 +0200

    add manifest entries back to version details

--- src/main/groovy/com/github/winplay02/gitcraft/meta/VersionDetails.java
+++ src/main/groovy/com/github/winplay02/gitcraft/meta/VersionDetails.java
@@ -1,7 +1,10 @@
 package com.github.winplay02.gitcraft.meta;
 
+import java.time.ZonedDateTime;
 import java.util.List;
 
 public record VersionDetails(String id, String normalizedVersion, List<String> next, List<String> previous,
-							 boolean client, boolean server, boolean sharedMappings) {
+							 List<ManifestEntry> manifests, boolean client, boolean server, boolean sharedMappings) {
+	public record ManifestEntry(String url, String type, ZonedDateTime time, ZonedDateTime lastModified, String hash, int downloadsId) {
+	}
 }
