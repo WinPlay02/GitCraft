ac599b816b4fee7894ddedd1f304521fece332cc
WinPlay02 <winplay02_gh@woberlaender.de>
2025-09-01 16:33:37 +0200

    ADDED: Initial implementation of classfile transformer for legacy launching

--- src/launcher_agent/java/com/github/winplay02/gitcraft/launchagent/GitCraftLauncherTransformer.java
+++ src/launcher_agent/java/com/github/winplay02/gitcraft/launchagent/GitCraftLauncherTransformer.java
@@ -1,5 +1,11 @@
 package com.github.winplay02.gitcraft.launchagent;
 
+import java.lang.classfile.ClassFile;
+import java.lang.classfile.ClassModel;
+import java.lang.classfile.ClassTransform;
+import java.lang.classfile.CodeTransform;
+import java.lang.constant.ClassDesc;
+import java.lang.constant.MethodTypeDesc;
 import java.lang.instrument.ClassFileTransformer;
 import java.lang.instrument.IllegalClassFormatException;
 import java.security.ProtectionDomain;
@@ -31,6 +37,44 @@ public class GitCraftLauncherTransformer implements ClassFileTransformer {
 	}
 
 	protected byte[] transform_net$minecraft$launchwrapper$Launch(byte[] classfileBuffer) {
-
+		ClassFile classFile = ClassFile.of();
+		ClassModel classModel = classFile.parse(classfileBuffer);
+		return classFile.transformClass(classModel, ClassTransform.transformingMethodBodies(methodModel -> methodModel.methodType().toString().equalsIgnoreCase("()V"), CodeTransform.ofStateful(() -> (builder, element) -> {
+			// constructor
+			builder.aload(0);
+			builder.invokespecial(classModel.superclass().orElseThrow().asSymbol(), "<init>", MethodTypeDesc.ofDescriptor("()V"));
+			// get class loader
+			builder.aload(0);
+			builder.invokevirtual(ClassDesc.ofInternalName("java/lang/Object"), "getClass", MethodTypeDesc.ofDescriptor("()Ljava/lang/Class;"));
+			builder.invokevirtual(ClassDesc.ofInternalName("java/lang/Class"), "getClassLoader", MethodTypeDesc.ofDescriptor("()Ljava/lang/ClassLoader;"));
+			builder.checkcast(ClassDesc.ofInternalName("jdk/internal/loader/BuiltinClassLoader"));
+			builder.astore(1);
+			// find field with reflection
+			builder.aload(1);
+			builder.invokevirtual(ClassDesc.ofInternalName("java/lang/Object"), "getClass", MethodTypeDesc.ofDescriptor("()Ljava/lang/Class;"));
+			builder.ldc("ucp");
+			builder.invokevirtual(ClassDesc.ofInternalName("java/lang/Class"), "getDeclaredField", MethodTypeDesc.ofDescriptor("(Ljava/lang/String;)Ljava/lang/reflect/Field;"));
+			builder.astore(2);
+			// make field accessible
+			builder.aload(2);
+			builder.loadConstant(1);
+			builder.invokevirtual(ClassDesc.ofInternalName("java/lang/reflect/Field"), "setAccessible", MethodTypeDesc.ofDescriptor("(Z)V"));
+			// access field
+			builder.aload(2);
+			builder.aload(1);
+			builder.invokevirtual(ClassDesc.ofInternalName("java/lang/reflect/Field"), "get", MethodTypeDesc.ofDescriptor("(Ljava/lang/Object;)Ljava/lang/Object;"));
+			builder.checkcast(ClassDesc.ofInternalName("jdk/internal/loader/URLClassPath"));
+			builder.astore(3);
+			// access URLs
+			builder.aload(3);
+			builder.invokevirtual(ClassDesc.ofInternalName("jdk/internal/loader/URLClassPath"), "getURLs", MethodTypeDesc.ofDescriptor("()[Ljava/net/URL;"));
+			builder.astore(4);
+			builder.new_(ClassDesc.ofInternalName("net/minecraft/launchwrapper/LaunchClassLoader"));
+			builder.dup();
+			builder.aload(4);
+			builder.invokespecial(ClassDesc.ofInternalName("net/minecraft/launchwrapper/LaunchClassLoader"), "<init>", MethodTypeDesc.ofDescriptor("([Ljava/net/URL;)V"));
+			builder.putstatic(ClassDesc.ofInternalName("net/minecraft/launchwrapper/Launch"), "classLoader", ClassDesc.ofInternalName("net/minecraft/launchwrapper/LaunchClassLoader"));
+			builder.return_();
+		})));
 	}
 }
