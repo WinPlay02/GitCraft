08b909cfdc491077a3204a9c528e9e880b3ea7fd
Space Walker <spacedoesrs@gmail.com>
2025-09-17 11:22:16 +0200

    add omniarchive manifest source

--- src/main/groovy/com/github/winplay02/gitcraft/manifest/ManifestSource.java
+++ src/main/groovy/com/github/winplay02/gitcraft/manifest/ManifestSource.java
@@ -1,6 +1,7 @@
 package com.github.winplay02.gitcraft.manifest;
 
 import com.github.winplay02.gitcraft.manifest.historic.HistoricMojangLauncherMetadataProvider;
+import com.github.winplay02.gitcraft.manifest.omniarchive.OmniarchiveMetadataProvider;
 import com.github.winplay02.gitcraft.manifest.skyrising.SkyrisingMetadataProvider;
 import com.github.winplay02.gitcraft.manifest.ornithe.OrnitheMetadataProvider;
 import com.github.winplay02.gitcraft.manifest.vanilla.MojangLauncherMetadataProvider;
@@ -15,6 +16,7 @@ public enum ManifestSource {
 	MOJANG(MojangLauncherMetadataProvider::new),
 	SKYRISING(SkyrisingMetadataProvider::new),
 	ORNITHEMC(OrnitheMetadataProvider::new),
+	OMNIARCHIVE(OmniarchiveMetadataProvider::new),
 	MOJANG_HISTORIC(() -> new HistoricMojangLauncherMetadataProvider((MojangLauncherMetadataProvider) MOJANG.getMetadataProvider(), (SkyrisingMetadataProvider) SKYRISING.getMetadataProvider()));
 
 	private final LazyValue<? extends MetadataProvider<OrderedVersion>> metadataProvider;
---
+++ src/main/groovy/com/github/winplay02/gitcraft/manifest/omniarchive/OmniarchiveMetadataProvider.java
@@ -0,0 +1,40 @@
+package com.github.winplay02.gitcraft.manifest.omniarchive;
+
+import java.util.List;
+
+import com.github.winplay02.gitcraft.manifest.ManifestSource;
+import com.github.winplay02.gitcraft.manifest.vanilla.MojangLauncherMetadataProvider;
+import com.github.winplay02.gitcraft.types.OrderedVersion;
+
+public class OmniarchiveMetadataProvider extends MojangLauncherMetadataProvider {
+
+	public OmniarchiveMetadataProvider() {
+		super("https://meta.omniarchive.uk/v1/manifest.json");
+	}
+
+	@Override
+	public ManifestSource getSource() {
+		return ManifestSource.OMNIARCHIVE;
+	}
+
+	@Override
+	public String getName() {
+		return "Omniarchive Version Metadata (https://omniarchive.uk//)";
+	}
+
+	@Override
+	public String getInternalName() {
+		return "omniarchive";
+	}
+
+	@Override
+	public List<String> getParentVersion(OrderedVersion mcVersion) {
+		switch (mcVersion.semanticVersion()) {
+		case "1.14-alpha.19.13.shareware" -> {
+			return List.of("1.14-alpha.19.13.b+1653");
+		}
+		}
+
+		return super.getParentVersion(mcVersion);
+	}
+}
