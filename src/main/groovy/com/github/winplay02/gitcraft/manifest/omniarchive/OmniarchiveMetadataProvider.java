15f96ebe77dab50ec4c9fc24ab0a8f59a351b7a6
0x189D7997 <199489335+0x189D7997@users.noreply.github.com>
2025-12-29 00:00:30 +0000

    Parent versions

--- src/main/groovy/com/github/winplay02/gitcraft/manifest/omniarchive/OmniarchiveMetadataProvider.java
+++ src/main/groovy/com/github/winplay02/gitcraft/manifest/omniarchive/OmniarchiveMetadataProvider.java
@@ -29,9 +29,111 @@ public class OmniarchiveMetadataProvider extends MojangLauncherMetadataProvider 
 	@Override
 	public List<String> getParentVersionIds(String versionId) {
 		switch (versionId) {
-		case "3D Shareware v1.34" -> {
-			return List.of("19w13b+1653");
-		}
+			// Combat
+			case "combat1" -> {
+				return List.of("1.14.3-pre4");
+			}
+			case "combat2" -> {
+				return List.of("combat1", "1.14.4");
+			}
+			case "combat3" -> {
+				return List.of("combat2");
+			}
+			case "combat4" -> {
+				return List.of("combat3", "1.15-pre3");
+			}
+			case "combat5" -> {
+				return List.of("combat4", "1.15.2-pre2");
+			}
+			case "combat6" -> {
+				return List.of("combat5", "1.16.2-pre3");
+			}
+			case "combat7" -> {
+				return List.of("combat6", "1.16.2");
+			}
+			case "combat7b" -> {
+				return List.of("combat7");
+			}
+			case "combat7c" -> {
+				return List.of("combat7b");
+			}
+			case "combat8" -> {
+				return List.of("combat7c");
+			}
+			case "combat8b" -> {
+				return List.of("combat8");
+			}
+			case "combat8c" -> {
+				return List.of("combat8b");
+			}
+			// Experimental 1.18
+			case "1.18-exp1" -> {
+				return List.of("1.17.1-pre1");
+			}
+			case "1.18-exp2" -> {
+				return List.of("1.18-exp1");
+			}
+			case "1.18-exp3" -> {
+				return List.of("1.18-exp2");
+			}
+			case "1.18-exp4" -> {
+				return List.of("1.18-exp3");
+			}
+			case "1.18-exp5" -> {
+				return List.of("1.18-exp4");
+			}
+			case "1.18-exp6" -> {
+				return List.of("1.18-exp5");
+			}
+			case "1.18-exp7" -> {
+				return List.of("1.18-exp6");
+			}
+			case "21w37a" -> {
+				return List.of("1.17.1", "1.18-exp7");
+			}
+			// Experimental 1.19
+			case "1.19-exp1" -> {
+				return List.of("1.18.1");
+			}
+			// April
+			case "2.0-preview" -> {
+				return List.of("1.5.1");
+			}
+			case "2.0-blue" -> {
+				return List.of("1.5.1");
+			}
+			case "2.0-purple" -> {
+				return List.of("1.5.1");
+			}
+			case "2.0-red" -> {
+				return List.of("1.5.1");
+			}
+			case "3D Shareware v1.34" -> {
+				return List.of("19w13b-1653");
+			}
+			case "23w13a_or_b-0722" -> {
+				return List.of("23w13a");
+			}
+			case "23w13a_or_b-1249" -> {
+				return List.of("23w13a_or_b-0722");
+			}
+			case "24w14potato-0838" -> {
+				return List.of("24w12a");
+			}
+			case "24w14potato-1104" -> {
+				return List.of("24w14potato-0838");
+			}
+			//Beta
+			case "b1.3-demo" -> {
+				return List.of("b1.3_01");
+			}
+			case "b1.2_02-dev" -> {
+				return List.of("b1.2_02");
+			}
+			//Classic
+			case "c0.30-c-1900-renew" -> {
+				return List.of("c0.30-c-1900");
+			}
 		}
 
 		return super.getParentVersionIds(versionId);
